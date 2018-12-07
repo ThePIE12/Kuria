fee72c1ad8c99a83406fec51c15188f0ef96ff46
ThePIE12 <Nicky01@live.se>
2018-12-07 09:11:13 +0100

    Added dotenv

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-const Commando = require('discord.js-commando');
+const Commando = require('discord.js-commando')
 const fs = require('fs');
+var dotenv = require('dotenv')
+dotenv.load()
 
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-const bot = new Commando.Client(config);
-const token = config.token;
 
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+const bot = new Commando.Client(config)
+const token = config.token
+
+bot.registry.registerGroup('random', 'Random')
+bot.registry.registerDefaults()
 bot.registry.registerCommandsIn(__dirname + '/commands')
 
 bot.on('message', (message) => {
-	const channel = message.channel;
+	const channel = message.channel
 	if (message.content.toLowerCase() == 'hello kuria') {
-		channel.send(`Hello ${message.author}...`);
+		channel.send(`Hello ${message.author}...`)
 	}
 
 	else if (message.content.toLowerCase() == 'nurupo') {
 		channel.send('Gah!')
 	}
-});
+})
 
 bot.on('ready', () => {
-	console.log("Ready!");
-});
+	console.log("Ready!")
+})
 
-bot.login(config.token);
+bot.login(process.env.TOKEN)
